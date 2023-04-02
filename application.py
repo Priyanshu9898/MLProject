b02818e80a68c8e59d9eae0b948d3d9dc406ef10
Priyanshu9898 <priyanshumalaviya17@gmail.com>
2023-04-02 15:57:21 +0530

    changes in flask app

--- application.py
+++ application.py
@@ -15,11 +15,10 @@ def index():
     return render_template('home.html') 
 
 
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/predictdata',methods=['POST'])
 def predict_datapoint():
-    if request.method=='GET':
-        return render_template('home.html')
-    else:
+    if request.method=='POST':
+        
         data=CustomData(
             gender=request.form.get('gender'),
             race_ethnicity=request.form.get('ethnicity'),
@@ -31,13 +30,13 @@ def predict_datapoint():
 
         )
         pred_df=data.get_data_as_data_frame()
-        print(pred_df)
+        # print(pred_df)
         print("Before Prediction")
 
         predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
+        # print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
+        # print("after Prediction")
         return render_template('home.html',results=results[0])
     
 
